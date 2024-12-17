536f88d16f084c0131a58db1af995e0b819f6512
Ali Armağan Uygun <75046911+aliarmgnuygun@users.noreply.github.com>
2024-12-17 13:16:58 +0300

    Delete LibraryManagementSystem/LibraryManagementSystem.Shared directory

--- LibraryManagementSystem/LibraryManagementSystem.Shared/AppRoles/AppRole.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibraryManagementSystem.Shared.AppRoles
-{
-    public static class AppRole
-    {
-        public const string Admin = "Admin";
-        public const string Librarian = "Librarian";
-        public const string Member = "Member";
-    }
-}
